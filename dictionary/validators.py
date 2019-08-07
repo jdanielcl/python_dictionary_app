afb053545427fb9755bed6bd15f47965c4bdeb01
home_pc <jdanielcarranzal@hotmail.com>
2019-08-07 16:36:41 -0500

    fix: the alphabetical validator allows - and spaces

--- dictionary/validators.py
+++ dictionary/validators.py
@@ -2,5 +2,6 @@ from django.core.exceptions import ValidationError
 
 
 def is_alphabetical(input_string):
-    if not input_string.strip().isalpha():
+    preprocessed_input = input_string.replace(' ', '').replace('-', '')
+    if not preprocessed_input.isalpha():
         raise ValidationError('Only characters allowed')
