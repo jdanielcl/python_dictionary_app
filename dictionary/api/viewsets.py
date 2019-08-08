1003136bb80ff89ab00f980d557a57435188f0b2
home_pc <jdanielcarranzal@hotmail.com>
2019-08-08 13:39:54 -0500

    add: a funtionality to search words meaning is added

--- dictionary/api/viewsets.py
+++ dictionary/api/viewsets.py
@@ -1,9 +1,22 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, views
+from rest_framework.response import Response
 from dictionary.models import Word
 from dictionary.api.serializers import WordSerializer
+from dictionary.forms import WordForm
+import requests as req
+import json
 
 
 class WordViewSet(viewsets.ModelViewSet):
 
     queryset = Word.objects.all()
     serializer_class = WordSerializer
+
+
+class WordSearch(views.APIView):
+
+    def get(self, request):
+        word = request.GET.get('word', None)
+        word_definition = req.get(f'https://googledictionaryapi.eu-gb.mybluemix.net/?define={word}&lang=en')
+        data = json.loads(word_definition.text)
+        return Response(data)
--- my_dictionary/urls.py
+++ my_dictionary/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .routers import router
+from dictionary.api.viewsets import WordSearch
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls), name='api'),
+    path('search/', WordSearch.as_view(), name='search'),
 ]
