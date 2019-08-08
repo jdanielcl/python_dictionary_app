a343568935f81ef70c32e13e0a624348f608d045
home_pc <jdanielcarranzal@hotmail.com>
2019-08-08 18:34:02 -0500

    Added Attemps model to count the accurracy of the user

--- dictionary/admin.py
+++ dictionary/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from dictionary.models import Word
+from dictionary.models import Word, Attempts
 
 # Register your models here.
 admin.site.register(Word)
+admin.site.register(Attempts)
--- dictionary/models.py
+++ dictionary/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from dictionary.validators import is_alphabetical
+from django.contrib.auth.models import User
 
 
 class Word(models.Model):
     name = models.CharField(max_length=40, unique=True, validators=[is_alphabetical, ])
+    users = models.ManyToManyField(User, through='Attempts')
 
     def __str__(self):
         return self.name
@@ -11,3 +13,20 @@ class Word(models.Model):
     def save(self, *args, **kwargs):
         is_alphabetical(self.name)
         super(Word, self).save(*args, **kwargs)
+
+
+class Attempts(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    added_date = models.DateTimeField(auto_now_add=True)
+    attempts = models.IntegerField(default='0')
+    hits = models.IntegerField(default=0)
+
+    @property
+    def accuracy(self):
+        try:
+            accuracy = self.hits*100/self.attempts
+        except ZeroDivisionError:
+            accuracy = 0
+        return accuracy
+
